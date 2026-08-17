2b67d519b205927fadc7a3f0510807c914fba24a
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-17 11:20:50 +0400

    Student Line no.7

--- src/entities/Student.java
+++ src/entities/Student.java
@@ -1,9 +1,12 @@
 package entities;
 
+import java.util.List;
+
 public class Student extends Person  {
     private String gradeLevel;
     private String enrollmentDate;
     private double feeBalance;
+    private List<String> subjects;
 
 
 
