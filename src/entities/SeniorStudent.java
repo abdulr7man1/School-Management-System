38133d27003b551977ec40fc6404fe367d9a4e5b
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-18 15:59:29 +0400

    SeniorStudent Line no.20

--- src/entities/SeniorStudent.java
+++ src/entities/SeniorStudent.java
@@ -17,7 +17,7 @@ public class SeniorStudent extends Student {
     public String getMajor() { return major; }
     public double getGpa() { return gpa; }
     public void setMajor(String major) { this.major = major; }
-
+    public void setGpa(double gpa) { this.gpa = gpa; }
 
 
 
