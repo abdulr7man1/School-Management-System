104de74f2ffa47aa78e3a48de3e0d811396f54b6
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-18 08:49:21 +0400

    Student Line no.67

--- src/entities/Student.java
+++ src/entities/Student.java
@@ -2,7 +2,7 @@ package entities;
 
 import java.util.List;
 
-public class Student extends Person  {
+public class Student extends Person {
     private String gradeLevel;
     private String enrollmentDate;
     private double feeBalance;
@@ -58,13 +58,16 @@ public class Student extends Person  {
     }
 
     public void setFeeBalance(double feeBalance) {
-            if (feeBalance < 0) {
-                System.out.println("Fee cannot be below 0. Keeping it at 0.");
-                this.feeBalance = 0;
-                return;
+        if (feeBalance < 0) {
+            System.out.println("Fee cannot be below 0. Keeping it at 0.");
+            this.feeBalance = 0;
+            return;
+        }
+        this.feeBalance = feeBalance;
     }
 
-    public void setSubjects(List<String> subjects) {
-        this.subjects = subjects;
+        public void setSubjects (List < String > subjects) {
+                this.subjects = subjects;
+            }
+        }
     }
-}
