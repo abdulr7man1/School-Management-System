e80f9d4c1958f0e38b58f16759aeb1edc142c654
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-18 15:54:28 +0400

    Person Line no.2

--- src/entities/SeniorStudent.java
+++ src/entities/SeniorStudent.java
@@ -1,4 +1,29 @@
 package entities;
 
-public class SeniorStudent {
+//
+public class SeniorStudent extends Student {
+    private String major;
+    private double gpa;
+
+    public SeniorStudent(String id, String firstName, String lastName, String dateOfBirth,
+                         String gender, String phoneNumber, String email, String address,
+                         String gradeLevel, String enrollmentDate, double feeBalance,
+                         String major, double gpa) {
+
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address,
+                gradeLevel, enrollmentDate, feeBalance);
+        this.major = major;
+        this.gpa = gpa;
+    }
+
+    public String getMajor() { return major; }
+    public double getGpa() { return gpa; }
+    public void setMajor(String major) { this.major = major; }
+    public void setGpa(double gpa) { this.gpa = gpa; }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Senior Student: " + getFirstName() + " " + getLastName()
+                + " | major: " + major + " | GPA: " + gpa);
+    }
 }
