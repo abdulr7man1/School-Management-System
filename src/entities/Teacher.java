5e05035a15292b7437f62ca77eb7d187337649f9
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-18 14:36:01 +0400

    Teacher Line no.7

--- src/entities/Teacher.java
+++ src/entities/Teacher.java
@@ -1,8 +1,11 @@
 package entities;
 
+import java.util.List;
+
 public class Teacher extends Person{
     private String subject;
     private int experienceYears;
     private double salary;
+    private List<String> classesTaught;
 
 }
